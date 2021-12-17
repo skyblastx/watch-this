01dbd824886a07c90f753ca5460b64c26bccfabc
Skyblastx <teckchenglow@gmail.com>
2021-12-17 17:27:19 +0800

    Update demopage.dart
    + Update UI of tab bar

--- lib/demopage.dart
+++ lib/demopage.dart
@@ -7,26 +7,77 @@ class TabBarDemo extends StatelessWidget{
   Widget build(BuildContext context)
   {
     return MaterialApp(
-      home:  DefaultTabController(
+      home: DefaultTabController(
         length: 3,
-          child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          bottom: const TabBar(
-            tabs: [
-              Tab(icon:Icon(Icons.access_time)),
-              Tab(icon:Icon(Icons.ten_k)),
-              Tab(icon:Icon(Icons.access_alarms)),
+        child: Scaffold(
+          appBar: AppBar(
+            leading: const Icon(Icons.access_time, color: Colors.black),
+            title: const Text("Trending"),
+            titleTextStyle: const TextStyle(color: Colors.black, fontSize: 30),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: const [
+              Icon(Icons.ten_k_sharp, color: Colors.black),
+              Icon(Icons.ten_k_sharp, color: Colors.black)
+            ],
+            bottom: TabBar(
+              isScrollable: true,
+              unselectedLabelColor: Colors.purpleAccent,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.purpleAccent),
+              tabs: [
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(color: Colors.purpleAccent, width: 1)
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.access_time),
+                      widthFactor: 3
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(color: Colors.purpleAccent, width: 1)
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.ten_k),
+                      widthFactor: 3,
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(color: Colors.purpleAccent, width: 1)
+                    ),
+                    child: const Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.access_alarms),
+                      widthFactor: 3,
+                    ),
+                  ),
+                ),
+              ],
+              )
+            ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.ac_unit_sharp),
+              Icon(Icons.ten_k),
+              Icon(Icons.directions_bike)
             ],
           ),
-          title: const Text('Tabs Demo 1'),
         ),
-          body:const TabBarView(children: [
-            Icon(Icons.ac_unit_sharp),
-            Icon(Icons.ten_k),
-            Icon(Icons.directions_bike)
-          ])
-        )
       )
     );
   }
